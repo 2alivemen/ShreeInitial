b35a4f28c8672de8effac670ff17ed9c5bdbd667
Shreesha <shreeshhegde007@gmail.com>
2023-01-20 00:07:46 +0530

    Improved Styling

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -21,7 +21,8 @@ function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        <div style={{height: '50px'}}>
+        // style={{height: '50px'}} 
+        <div className='tw-h-16'>
         {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
            <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg} 
         </div>}
